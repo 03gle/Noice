b8dedc49a2e24d931def0b89cfacd9bcc5fd4f48
Mathias Schifter <mathias@schifter.dev>
2022-10-02 18:30:50 +0200

    input and visuals

--- Assets/Scripts/InputTracker.cs
+++ Assets/Scripts/InputTracker.cs
@@ -2,96 +2,145 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Windows.Forms.DataVisualization.Charting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputTracker : MonoBehaviour
 {
     public int radius = 10;
 
-    public List<TrackingPoint> trackingPointsLH;
-    public List<TrackingPoint> trackingPointsRH;
+    public List<TrackingPoint> trackingPointsL;
+    public List<TrackingPoint> trackingPointsR;
     public List<TrackingPoint> trackingPointsE;
 
     public Transform leftController;
     public Transform rightController;
     public Transform eyes;
 
-    private Vector3 hitLH;
-    private Vector3 hitRH;
+    private Vector3 hitL;
+    private Vector3 hitR;
     private Vector3 hitE;
+
+    private int layerMask = 1 << 11;
 
     public int pointInfluenceLimit = 2;
 
-    private FMOD.Studio.EventInstance eventInstanceLH;
-    public FMODUnity.EventReference eventRefLH;
+    private FMOD.Studio.EventInstance eventInstanceL;
+    public FMODUnity.EventReference eventRefL;
 
+    private FMOD.Studio.EventInstance eventInstanceR;
+    public FMODUnity.EventReference eventRefR;
+
+    private FMOD.Studio.EventInstance eventInstanceE;
+    public FMODUnity.EventReference eventRefE;
+
+
+    public InputActionReference triggerL;
+    public InputActionReference triggerR;
 
     private void Start()
     {
-        eventInstanceLH = FMODUnity.RuntimeManager.CreateInstance(eventRefLH);
+        eventInstanceL = FMODUnity.RuntimeManager.CreateInstance(eventRefL);
+        eventInstanceL.start();
+
+        eventInstanceR = FMODUnity.RuntimeManager.CreateInstance(eventRefR);
+        eventInstanceR.start();
+
+        //eventInstanceE = FMODUnity.RuntimeManager.CreateInstance(eventRefE);
+        //eventInstanceE.start();
     }
 
     private void Update()
     {
-        int layerMask = 1 << 11;
+        float eL = triggerL.action.ReadValue<float>();
+        eventInstanceL.setParameterByName("L_Effect", eL);
+
+        float eR = triggerR.action.ReadValue<float>();
+        eventInstanceR.setParameterByName("R_Effect", eR);
+        
         RaycastHit hit;
 
         // Left hand
         if (Physics.Raycast(leftController.position, leftController.forward, out hit, 100, layerMask))
         {
-            hitLH = hit.point;
+            hitL = hit.point;
             Debug.DrawRay(leftController.position, leftController.forward * hit.distance, Color.red);
 
-            foreach (TrackingPoint tp in trackingPointsLH)
+            foreach (TrackingPoint tp in trackingPointsL)
             {
-                tp.distFromHit = ArchDistance(hitLH, tp.transform.position);
+                tp.distFromHit = ArchDistance(hitL, tp.transform.position);
             }
 
-            trackingPointsLH.Sort((p1, p2) => p1.distFromHit.CompareTo(p2.distFromHit));
+            trackingPointsL.Sort((p1, p2) => p1.distFromHit.CompareTo(p2.distFromHit));
 
-            for (int i = 0; i < trackingPointsLH.Count; i++)
+            for (int i = 0; i < trackingPointsL.Count; i++)
             {
                 if (i < pointInfluenceLimit)
                 {
-                    Debug.Log("Left Hand " + i + ": " + Blend(trackingPointsLH, i));
-                    eventInstanceLH.setParameterByName(trackingPointsLH[i].parameterName, Blend(trackingPointsLH, i));
+                    //Debug.Log("Left Hand " + i + ": " + Blend(trackingPointsLH, i));
+                    var res = eventInstanceL.setParameterByName(trackingPointsL[i].parameterName, Blend(trackingPointsL, i), true);
+                    //Debug.Log(res);
                 }
                 else
                 {
-                    Debug.Log("Left Hand " + i + ": 0");
-                    eventInstanceLH.setParameterByName(trackingPointsLH[i].parameterName, 0);
+                    //Debug.Log("Left Hand " + i + ": 0");
+                    eventInstanceL.setParameterByName(trackingPointsL[i].parameterName, 0, true);
                 }
             }
             
         }
 
-        //// Right hand
-        //if (Physics.Raycast(rightController.position, rightController.forward, out hit, 100, layerMask))
-        //{
-        //    hitRH = hit.point;
-        //    Debug.DrawRay(rightController.position, rightController.forward * hit.distance, Color.green);
+        // Right hand
+        if (Physics.Raycast(rightController.position, rightController.forward, out hit, 100, layerMask))
+        {
+            hitR = hit.point;
+            Debug.DrawRay(rightController.position, rightController.forward * hit.distance, Color.green);
 
-        //    foreach (TrackingPoint tp in trackingPointsRH)
-        //    {
-        //        tp.distFromHit = ArchDistance(hitRH, tp.transform.position);
-        //    }
+            foreach (TrackingPoint tp in trackingPointsR)
+            {
+                tp.distFromHit = ArchDistance(hitR, tp.transform.position);
+            }
 
-        //    trackingPointsRH.Sort((p1, p2) => p1.distFromHit.CompareTo(p2.distFromHit));
-        //}
-        
+            trackingPointsR.Sort((p1, p2) => p1.distFromHit.CompareTo(p2.distFromHit));
 
-        //// Eyes
-        //if (Physics.Raycast(eyes.position, eyes.forward, out hit, 100, layerMask))
-        //{
-        //    hitE = hit.point;
-        //    Debug.DrawRay(eyes.position, eyes.forward * hit.distance, Color.blue);
+            for (int i = 0; i < trackingPointsR.Count; i++)
+            {
+                if (i < pointInfluenceLimit)
+                {
+                    var res = eventInstanceR.setParameterByName(trackingPointsR[i].parameterName, Blend(trackingPointsR, i), true);
+                }
+                else
+                {
+                    eventInstanceR.setParameterByName(trackingPointsR[i].parameterName, 0, true);
+                }
+            }
+        }
 
-        //    foreach (TrackingPoint tp in trackingPointsE)
-        //    {
-        //        tp.distFromHit = ArchDistance(hitE, tp.transform.position);
-        //    }
 
-        //    trackingPointsE.Sort((p1, p2) => p1.distFromHit.CompareTo(p2.distFromHit));
-        //}
+        // Eyes
+        if (Physics.Raycast(eyes.position, eyes.forward, out hit, 100, layerMask))
+        {
+            hitE = hit.point;
+            Debug.DrawRay(eyes.position, eyes.forward * hit.distance, Color.blue);
+
+            foreach (TrackingPoint tp in trackingPointsE)
+            {
+                tp.distFromHit = ArchDistance(hitE, tp.transform.position);
+            }
+
+            trackingPointsE.Sort((p1, p2) => p1.distFromHit.CompareTo(p2.distFromHit));
+
+            for (int i = 0; i < trackingPointsE.Count; i++)
+            {
+                if (i < pointInfluenceLimit)
+                {
+                    var res = eventInstanceE.setParameterByName(trackingPointsE[i].parameterName, Blend(trackingPointsE, i), true);
+                }
+                else
+                {
+                    eventInstanceR.setParameterByName(trackingPointsE[i].parameterName, 0, true);
+                }
+            }
+        }
 
     }
 
@@ -114,9 +163,9 @@ public class InputTracker : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(hitLH, 0.2f);
+        Gizmos.DrawSphere(hitL, 0.2f);
         Gizmos.color = Color.green;
-        Gizmos.DrawSphere(hitRH, 0.2f);
+        Gizmos.DrawSphere(hitR, 0.2f);
         Gizmos.color = Color.blue;
         Gizmos.DrawSphere(hitE, 0.2f);
     }
